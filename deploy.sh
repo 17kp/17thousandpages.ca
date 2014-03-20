daafcea35d591404b631bb356b7e5f89cd6d41ad
Thomas J Bradley <hey@thomasjbradley.ca>
2014-03-20 16:53:32 -0400

    Compressed images.

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-rsync -e ssh -a -r --delete --delete-excluded --progress --compress --chmod=u=rwx,go=rx --exclude-from 'rsync-exclude.txt' ./ thousand:/home/thousand/public_html
+rsync -e ssh -a -r --delete --delete-excluded --progress --compress --chmod=u=rwx,go=rx --exclude-from 'rsync-exclude.txt' ./_site thousand:/home/thousand/public_html
